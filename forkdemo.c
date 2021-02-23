53d4f15e9bb4c3fc866989cc0779d344b63c7309
Akshaypatil2006 <79127539+Akshaypatil2006@users.noreply.github.com>
2021-02-23 15:05:20 +0530

    Update forkdemo.c
    fork updated

--- forkdemo.c
+++ forkdemo.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+int main() 
+{ 
+  int pid=0;
+  
+    // make two process which run same 
+    // program after this instruction 
+  pid =  fork(); 
+  if(pid==0)
+  {
+	  printf("in child");
+  }
+  elseprintf("in paraent");
+  
+    printf("Hello world!\n"); 
+    return 0; 
+}
